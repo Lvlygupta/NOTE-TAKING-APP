9632069ceaff61b47f8bb3cc3bc7dc280c7a92fd
L0VELY GUPTA <108621050+Lvlygupta@users.noreply.github.com>
2022-11-29 18:47:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-console.log("Hey this is the note taking app!")
+console.log("Hey this is the note taking app!");
 
-
-a=3;
-console.log(a);
 showNotes();
+let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click",function(){
-    let addTxt=document.getElementById("addTxt");
-    let addTitle = document.getElementById("addTitle");
-    let notes = localStorage.getItem("notes");
-    let notesObj=[];
+addBtn.addEventListener("click", function () {
+  let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
+
+  let notes = localStorage.getItem("notes");
+  let num = 1;
+  if (notes == null) {
+    notesObj = [];
+  num  = 1;
+
+  } else if (addTitle.value == "") {
+    num=0;
+    alert("Add some title!");
+  } else if (addTxt.textLength == 0 || addTitle.textLength == 0) {
+    num = 0;
+    alert("Add some content!");
+  } else {
+    num = 1;
+    notesObj = JSON.parse(notes);
     
-    if(notes==null){
-        notesObj=[];
-    }
-    else if(addTitle.value == ""){
-       alert("Add some title!");
-   }
-     else if(addTxt.textLength == 0 || addTitle.textLength == 0){
-        alert("Add some content!");
-    }
-    else{
-        notesObj=JSON.parse(notes);
+  }
+  if(num!=0){
     let myObj = {
         title: addTitle.value,
-        text: addTxt.value
-    }
+        text: addTxt.value,
+    };
     notesObj.push(myObj);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
-    addTxt.value="";
-    addTitle.value="";
-    showNotes();}
-})
-function showNotes(){
-    let notes = localStorage.getItem("notes");
-    if(notes==null){
-        notesObj=[];
-    } else{
-        notesObj=JSON.parse(notes);
+    
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    addTitle.value = "";
     }
-    let html="";
-    notesObj.forEach(function(element,index) {
-        html+=`
+    showNotes();
+});
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
         <div class=" Notescard my-2 mx-2 card" style="width: 18rem;">
                 
         <div class="card-body">
@@ -51,14 +57,13 @@ function showNotes(){
         </div>
 
       </div>
-        `
-    });
-    let notesElm = document.getElementById("notes");
-    if(notesObj.length !=0){
-        notesElm.innerHTML = html;
-    }
-    else{
-        notesElm.innerHTML = `
+        `;
+  });
+  let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `
         <div class=" Notescard my-2 mx-2 card" style="width: 18rem;">
                 
         <div class="card-body">
@@ -68,36 +73,32 @@ function showNotes(){
         </div>
 
       </div>
-        `
+        `;
+  }
+}
+
+function delNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  let notesElm = document.getElementById("notes");
+  showNotes();
+}
+let st = document.getElementById("searchTxt");
+st.addEventListener("input", function () {
+  let inputVal = st.value.toLowerCase();
+  let noteCards = document.getElementsByClassName("Notescard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
     }
-}
-
-function delNote(index){
-
-    let notes = localStorage.getItem("notes");
-    if(notes==null){
-        notesObj=[];
-    } else{
-        notesObj=JSON.parse(notes);
-        }
-    notesObj.splice(index,1);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
-    let notesElm = document.getElementById("notes");
-    showNotes();
-}
-let st=document.getElementById("searchTxt");
-st.addEventListener("input",function(){
-    let inputVal = st.value.toLowerCase();
-    let noteCards = document.getElementsByClassName("Notescard");
-    Array.from(noteCards).forEach(function(element){
-       
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none"
-        }
-    })
-})
-
+  });
+});
